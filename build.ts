3d07cf5d22c44772b74c477a96fb111611a0d446
Eduardo Velho <eduardo.velho@icloud.com>
2022-10-18 21:39:16 -0300

    cats commit

--- build.ts
+++ build.ts
@@ -1,17 +1,84 @@
 import shell from "shelljs";
 import appConfig from "./app.json";
+import { createWriteStream } from "fs";
 import fs from "fs/promises";
+import path from "path";
+import { faker } from "@faker-js/faker";
+import { https } from "follow-redirects";
+import sharp from "sharp";
+
+function generateImages(amount: number = 5) {
+  return Array.from({ length: amount }, () => faker.image.cats());
+}
+
+async function processImage(
+  url: string,
+  imagePath: string,
+  thumbnailPath: string
+) {
+  const imageStream = createWriteStream(imagePath);
+  const thumbnailStream = createWriteStream(thumbnailPath);
+  https.get(url, (request) => {
+    request.pipe(imageStream);
+    const sharpInstance = sharp();
+    sharpInstance
+      .resize(200, 200, {
+        fit: sharp.fit.cover,
+      })
+      .pipe(thumbnailStream);
+
+    imageStream.on("finish", () => {
+      imageStream.close();
+    });
+
+    request.pipe(sharpInstance);
+
+    thumbnailStream.on("finish", () => {
+      thumbnailStream.close();
+    });
+  });
+}
 
 (async function build() {
   console.log("Starting build process...");
   shell.rm("-rf", "public");
   shell.mkdir("public");
+  shell.mkdir(path.join("public", "images"));
+  shell.mkdir(path.join("public", "thumbnails"));
+
+  console.log("Downloading images...");
+  const imagesUrls = generateImages(10);
+  await Promise.all(
+    imagesUrls.map((imageUrl, index) =>
+      processImage(
+        imageUrl,
+        path.join("public", "images", `${index}.jpg`),
+        path.join("public", "thumbnails", `${index}.jpg`)
+      )
+    )
+  );
+
+  const imagesContent = imagesUrls
+    .map(
+      (_, index) => `
+    <a href="/jamstack-teste/images/${index}.jpg">
+        <img src="/jamstack-teste/thumbnails/${index}.jpg">
+    </a>
+  `
+    )
+    .join("");
+
+  const imagesGrid = `
+        <div class="images-grid">
+            ${imagesContent}
+        </div>
+  `;
 
   const htmlFile = (await fs.readFile("index.html"))
     .toString()
     .replace("$TITLE", appConfig.title)
     .replace("$DESCRIPTION", appConfig.description)
-    .replace("$CONTENT", appConfig.content);
+    .replace("$CONTENT", imagesGrid);
 
   await fs.writeFile("public/index.html", htmlFile);
   console.log("Success!");
